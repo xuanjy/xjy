4d2b8978bd54d81045dc4f3306f98ae4e4f705eb
xjy <626052799@qq.com>
2020-02-05 15:47:32 +0800

    init repo

---
+++ src/main/java/com/xjy/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package com.xjy.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 黄粱一梦
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
